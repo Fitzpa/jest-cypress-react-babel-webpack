62a96751c24f834c41ccc0cf9b4f6ecc8ab7535f
Louie Fitzpatrick <fitz4492@gmail.com>
2020-06-16 22:30:47 -0500

    configured Jest to use identity-obj-proxy when using CSS Modules

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
+    '\\.module\\.css$': 'identity-obj-proxy',
     '\\.css$': require.resolve('./test/style-mock.js'),
   },
 }
 
 // if we were not testing things that needs a dom tree then we could tell jest we want to use node instead of jsdom and save some time test running
+
+// if you are using css modules tell jest that everything that uses .module.css to instead import identity-obj-proxy, which will return a string after styles.
+// ex. className={styles.autoScalingText}   will read as   calss="autoScalingText"
