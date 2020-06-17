6b865836bc7cdd151649cecbc8e88d7faaf78ace
Louie Fitzpatrick <fitz4492@gmail.com>
2020-06-16 22:21:50 -0500

    configured jest to support importing CSS files with Jests moduleNameMapper

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '\\.css$': require.resolve('./test/style-mock.js'),
+  },
 }
 
 // if we were not testing things that needs a dom tree then we could tell jest we want to use node instead of jsdom and save some time test running
